b56b860c8d527b862804f5059658e70c1d75ee56
bhavesh-amrutkar-dev <bhavesh.amrutkar.dev@gmail.com>
2026-09-25 16:35:19 +0530

    Sync 211. Palindrome Number [DSA] (TUFHub)

--- DSA/Beginner-Problems/palindrome-number/solution.cpp
+++ DSA/Beginner-Problems/palindrome-number/solution.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(int n) {
         int initialVal = n;
         int revNum  = 0;
-        n = abs(n);
+        // n = abs(n);
         while(n>0){
             revNum = (revNum*10) + (n%10);
             n/=10;
